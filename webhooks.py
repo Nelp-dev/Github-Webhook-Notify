af136ba5079a39bd669b0528be82024693486dab
cjh5414 <cjh5414@gmail.com>
2017-05-11 07:18:19 +0000

    Notify with line message when pull request is created

---
+++ webhooks.py
@@ -0,0 +1,46 @@
+from flask import Flask, request, jsonify
+import urllib3
+
+LINE_NOTIFY_URL = 'https://notify-api.line.me/api/notify'
+LINE_HEADERS = {
+    'Authorization' : 'Bearer ' + 'gakjPa74Fw2tlPavkCfeBaFmVPqiSWWjoUBlqKCiJoQ'
+}
+
+app = Flask(__name__)
+
+
+def notify_to_line(message):
+    try:
+        http = urllib3.PoolManager()
+        response = http.request(
+            'POST',
+            LINE_NOTIFY_URL,
+            headers=LINE_HEADERS,
+            fields={'message': message}
+        )
+        print('Response HTTP Status Code: {status_code}'.format(
+        status_code=response.status))
+    except urllib3.exceptions.NewConnectionError:
+        print('Connection failed.')
+
+
+def open_pull_request():
+    json_dict = request.get_json()
+    message = 'Open Pull Request ' + '#' + str(json_dict['number']) + '\n'
+    message += (json_dict['pull_request']['user']['login'] + '\n')
+    message += (json_dict['pull_request']['title'] + '\n')
+    message += (json_dict['pull_request']['html_url'] + '\n')
+    notify_to_line(message)
+
+
+@app.route('/postreceive', methods=['POST'])
+def hello_world():
+    if request.headers['X-GitHub-Event'] == 'pull_request' and request.get_json()['action'] == 'opened':
+        open_pull_request()
+        return 'success'
+    else:
+        return 'hello'
+
+
+if __name__ == '__main__':
+    app.run()
